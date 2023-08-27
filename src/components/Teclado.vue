154ef0bfd20e6ec5ee74d5c56c215a264b1d9211
mustache1up <daniel.sp@msn.com>
2023-08-27 02:44:04 -0300

    Teclado versao inicial

--- src/components/Teclado.vue
+++ src/components/Teclado.vue
@@ -1,128 +1,128 @@
 <template>
-    <div class="keyboard">
-      <div class="keyboard-row" v-for="(row, rowIndex) in buttonRows" :key="rowIndex">
-        <button
-          v-for="(btn, btnIndex) in row" :key="btnIndex"
-          class="keyboard-key"
-          :data-status="btn.status"
-          :data-type="btn.action.name"
-          @click.prevent="btn.action.callable"
-          @mousedown.prevent=""
-        >
-          <span>
-            {{ btn.value }}
-          </span>
-        </button>
-      </div>
+  <div class="teclado">
+    <div v-for="(linha, indiceLinha) in buttonRows" :key="indiceLinha" class="teclado-linha">
+      <button v-for="(tecla, indiceTecla) in linha" :key="indiceTecla"
+        class="teclado-tecla"
+        :data-status="tecla.status"
+        :data-type="tecla.tipo"
+        @click.prevent="tecla.action"
+        @mousedown.prevent=""
+      >
+        {{ tecla.texto }}
+      </button>
     </div>
-  </template>
-  
-  <script>
- 
-    export default {
-      data() {
-        return {
-          lines: [
-            "QWERTYUIOP   ",
-            "ASDFGHJKL <",
-            "ZXCVBNM >"
-          ]
-        };
-      },
-  
-      computed: {
-        buttonRows() {
-          return this.lines.map(line => {
-            return line.split("").map(char => {
-              if (char === " ") {
-                return {
-                  action: { name: "espacador", callable: null },
-                  value: null,
-                };
-              } else if (char === ">") {
-                return {
-                  action: { name: "enviar", callable: this.enviar },
-                  value: "enviar",
-                };
-              } else if (char === "<") {
-                return  {
-                  action: { name: "backspace", callable: this.backspace },
-                  value: "<",
-                };
-              } else {
-                return {
-                  action: { name: null, callable: this.letra.bind(this, char) },
-                  value: char,
-                  status: "correta",
-                };
-              }
-            });
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        linhasLayout: [
+          "QWERTYUIOP   ",
+          "ASDFGHJKL <",
+          "ZXCVBNM >"
+        ]
+      };
+    },
+
+    computed: {
+      buttonRows() {
+        return this.linhasLayout.map(linhaTeclas => {
+          return linhaTeclas.split("").map(caractere => {
+            if (caractere === " ") {
+              return {
+                tipo: "espacador",
+                acao: null,
+                texto: null,
+              };
+            } else if (caractere === ">") {
+              return {
+                tipo: "enviar",
+                acao: this.enviar,
+                texto: "enviar",
+              };
+            } else if (caractere === "<") {
+              return  {
+                tipo: "backspace",
+                acao: this.backspace,
+                texto: "<",
+              };
+            } else {
+              return {
+                tipo: null,
+                acao: this.letra.bind(this, caractere),
+                texto: caractere,
+                status: "desconhecido",
+              };
+            }
           });
-        },
+        });
       },
-  
-      methods: {
-        letra(char) {
-          this.$emit("letra", char);
-        },
+    },
 
-        backspace() {
-          this.$emit("backspace");
-        },
+    methods: {
+      letra(caractere) {
+        this.$emit("letra", caractere);
+      },
 
-        enviar() {
-          this.$emit("enviar");
-        },
-      }
-    };
-  </script>
-  
-  <style>
-    .keyboard-row {
-      padding: 0;
-      margin: 0;
-      text-align: center;
-    }
-  
-    .keyboard-key {
-      border: none;
-      outline: none;
-      padding: 1px;
-      min-height: 40px;
-      min-width: 25px;
-      margin: 0 2px;
-      background: #EEE;
-      color: #666;
-      cursor: pointer;
-      font-family: inherit;
-      font-size: inherit;
-      border-radius: 2px;
-    }
-  
-    .keyboard-key:hover {
-      background: #E0E0E0;
-    }
-  
-    .keyboard-key:active {
-      background: #777;
-      color: #FFF;
-      box-shadow: inset 0 1px 4px rgba(#000, 0.1);
-    }
+      backspace() {
+        this.$emit("backspace");
+      },
 
-    .keyboard-key[data-type="enviar"] {
-      padding: 0 8px;
-    }  
-
-    .keyboard-key[data-type="espacador"] {
-      min-width: 2px;
-      width: 2px;
-      padding: 0;
-      background: none;
-      color: none;
+      enviar() {
+        this.$emit("enviar");
+      },
     }
+  };
+</script>
 
-    .keyboard-key[data-status="correta"] {
-      background: #7F7;
-      color: #000;
-    }
-  </style>
+<style>
+  .teclado-linha {
+    padding: 0;
+    margin: 0;
+    text-align: center;
+  }
+
+  .teclado-tecla {
+    border: none;
+    outline: none;
+    padding: 1px;
+    min-height: 40px;
+    min-width: 25px;
+    margin: 0 2px;
+    background: #EEE;
+    color: #666;
+    cursor: pointer;
+    font-family: inherit;
+    font-size: inherit;
+    border-radius: 2px;
+  }
+
+  .teclado-tecla:hover {
+    background: #E0E0E0;
+  }
+
+  .teclado-tecla:active {
+    background: #777;
+    color: #FFF;
+    box-shadow: inset 0 1px 4px rgba(#000, 0.1);
+  }
+
+  .teclado-tecla[data-type="enviar"] {
+    padding: 0 8px;
+  }  
+
+  .teclado-tecla[data-type="espacador"] {
+    min-width: 2px;
+    width: 2px;
+    padding: 0;
+    background: none;
+    color: none;
+  }
+
+  .teclado-tecla[data-status="correta"] {
+    background: #7F7;
+    color: #000;
+  }
+</style>
